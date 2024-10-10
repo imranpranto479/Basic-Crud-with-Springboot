0d4201b0d27e6cf5e57ada4e3a131a0419e8de1e
imranpranto479 <imranaj479@gmail.com>
2024-10-11 02:08:10 +0600

    created a basic MVC architecture project with GET and POST

---
+++ src/main/java/com/imranpranto/main/springmvc/controller/MainController.java
@@ -0,0 +1,59 @@
+package com.imranpranto.main.springmvc.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.imranpranto.main.springmvc.model.Programmer;
+
+@Controller
+@ControllerAdvice
+public class MainController {
+
+    @ModelAttribute
+    public void welcome(Model m) {
+        m.addAttribute("msg", "Welcome to Spring Boot Tutorial");
+    }
+
+    // @RequestMapping("/home")
+    @GetMapping("/home")
+    public String homePage() {
+        return "HomePage.html";
+    }
+
+    // @RequestMapping(value="/addProgrammer", method=RequestMethod.POST)
+    @PostMapping("/addProgrammer")
+    public String addProgrammer(
+            @ModelAttribute("p") Programmer programmer) { // for getting data @RequestParam
+
+        // ModelAndView mv =new ModelAndView();
+
+        // mv.setViewName("ProgrammerInfo.html");
+
+        // return mv;
+
+        // model.addAttribute("pName", n);
+        // model.addAttribute("pId", i);
+        // model.addAttribute("pLang", pLang);
+
+        return "ProgrammerInfo.html";
+    }
+
+    @GetMapping("/allProgrammer")
+    public String allProgrammer(Model m){
+        List<Programmer> p = new ArrayList<Programmer>();
+        p.add((new Programmer(101, "karim", "Java")));
+        p.add((new Programmer(102, "Rahim", "Python")));
+        p.add((new Programmer(103, "Momin", "C#")));
+
+        m.addAttribute("programmers", p);
+        return "AllProgrammer.html";
+    }
+}
